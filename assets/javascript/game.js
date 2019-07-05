6eac114b1c276c550142908395f76a279c33a0ce
Hari <augten95@gmail.com>
2019-07-05 11:42:01 -0500

    Displaying the value of the clicked crystal in a pop-up alert

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -39,3 +39,16 @@ for(var i = 0; i < numberOptions.length; i++){
     // Lastly, each crystal image (with all its classes and attributes) will get added to the page.
     $("#crystals").append(imageCrystal);
 }
+
+// This time, our click event applies to every single crystal on the page. Not just one.
+$(".crystal-image").on("click", function() {
+
+    // Determining the crystal's value requires us to extract the value from the data attribute.
+    // Using the $(this) keyword specifies that we should be extracting crystal value of the clicked crystal.
+    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter.
+
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    alert("You clicked: " + crystalValue);
+});
